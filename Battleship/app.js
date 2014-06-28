8717501eceb4f361b237c60a495c61454851949c
Vaishak <vaishak93@gmail.com>
2014-06-28 21:46:18 +0530

    added randomize

--- Battleship/app.js
+++ Battleship/app.js
@@ -1,6 +1,1 @@
-﻿/// <reference path="scripts/typings/angularjs/angular.d.ts" />
-var battleshipApp = angular.module('battleshipApp', ['ngRoute']).config(function ($routeProvider) {
-    $routeProvider.when('/floor', { templateUrl: 'floor.html', controller: 'FloorCtrl' }).otherwise({ redirectTo: 'floor.html' });
-}).controller('FloorCtrl', function ($scope) {
-});
-//# sourceMappingURL=app.js.map
+﻿//# sourceMappingURL=app.js.map
--- Battleship/screen.ts
+++ Battleship/screen.ts
@@ -9,12 +9,29 @@ module scrren {
     var queue: createjs.LoadQueue;
     var shouldUpdate: boolean = true;
     var cells: any[] = [];
+    var ships: Array<Ship> = new Array<Ship>(5);
     var manifest: Object = [{ src: "images/backgroundscreen.jpg", id: "backgroundScreen" },
                             { src: "images/aircraftCarrier.png", id: "aircraftcarrier" },
                             { src: "images/Destroyer.png", id: "destroyer" },
                             { src: "images/Submarine.png", id: "submarine" },
                             { src: "images/PatrolBoat.png", id: "patrolboat" },
                             { src: "images/grid.png", id: "grid" }];
+
+    class Ship {
+        public shipObject: createjs.Bitmap;
+        public length: number;
+        public row: number;
+        public column: number;
+        public isVertical: boolean;
+
+        constructor(shipObject: createjs.Bitmap, length: number) {
+            this.shipObject = shipObject;
+            this.length = length;
+            this.row = 0;
+            this.column = 0;
+            this.isVertical = false;
+        }
+    }
 
     function startPreload(): void {
         queue = new createjs.LoadQueue(false);
@@ -30,44 +47,34 @@ module scrren {
     }
 
     function handleComplete(eventinfo: CustomEvent): void {
-        var bg: createjs.Bitmap = new createjs.Bitmap(<HTMLImageElement>queue.getResult("backgroundScreen"));
-        var ships: Array<createjs.Bitmap> = new Array(5);
+        var bg: createjs.Bitmap = new createjs.Bitmap(<HTMLImageElement>queue.getResult("backgroundScreen"));        
         var aircraftCarrier: createjs.Bitmap = new createjs.Bitmap(<HTMLImageElement>queue.getResult("aircraftcarrier"));
-        ships[0] = aircraftCarrier;
-        var destroyer1: createjs.Bitmap = new createjs.Bitmap(<HTMLImageElement>queue.getResult("destroyer"));
-        ships[1] = destroyer1;
-        var destroyer2: createjs.Bitmap = new createjs.Bitmap(<HTMLImageElement>queue.getResult("destroyer"));
-        ships[2] = destroyer2;
-        var patrolBoat: createjs.Bitmap = new createjs.Bitmap(<HTMLImageElement>queue.getResult("patrolboat"));
-        ships[3] = patrolBoat;
+        ships[0] = new Ship(aircraftCarrier, 4);
         var submarine: createjs.Bitmap = new createjs.Bitmap(<HTMLImageElement>queue.getResult("submarine"));
-        ships[4] = submarine;
+        ships[1] = new Ship(submarine, 3);
+        var destroyer1: createjs.Bitmap = new createjs.Bitmap(<HTMLImageElement>queue.getResult("destroyer"));
+        ships[2] = new Ship(destroyer1, 2);
+        var destroyer2: createjs.Bitmap = new createjs.Bitmap(<HTMLImageElement>queue.getResult("destroyer"));
+        ships[3] = new Ship(destroyer2, 2);
+        var patrolBoat: createjs.Bitmap = new createjs.Bitmap(<HTMLImageElement>queue.getResult("patrolboat"));
+        ships[4] = new Ship(patrolBoat, 1);
+
         var grid: createjs.Bitmap = new createjs.Bitmap(<HTMLImageElement>queue.getResult("grid"));
 
-        // NOTE: Draggable aircraft carrier. Remove later.
-        aircraftCarrier.on("pressmove", function (evt: any) {
-            this.x = evt.stageX + this.offset.x;
-            this.y = evt.stageY + this.offset.y;
-            // indicate that the stage should be updated on the next tick:
-            shouldUpdate = true;
-        });
-
         var gridContainer: createjs.Container = new createjs.Container();
-        gridContainer.x = 200;
-        gridContainer.y = 150;
+        gridContainer.x = 100;
+        gridContainer.y = 100;
 
         cells = createGridCells();
 
-        for (var i = 0; i < 5; i++) {
-            cells[i][0].addChild(ships[i]);
-        }
+        randomlyPlaceShips();
 
-        rotate(ships[0], false);
-        rotate(ships[0], true);
+        rotate(ships[0]);
+        rotate(ships[0]);
 
         gridContainer.addChild(grid);
         for (var i = 0; i < 5; i++) {
-            for (var j = 0; j < 6; j++) {
+            for (var j = 0; j < 7; j++) {
                 gridContainer.addChild(cells[i][j]);
             }
         }        
@@ -87,7 +94,7 @@ module scrren {
         for (var i = 0; i < 5; i++) {
             cells[i] = new Array(6);
             x = 0;
-            for (var j = 0; j < 6; j++) {
+            for (var j = 0; j < 7; j++) {
                 var cell: createjs.Container = new createjs.Container();
                 cell.x = x;
                 cell.y = y;
@@ -99,15 +106,79 @@ module scrren {
         return cells;
     }
 
-    function rotate(ship: createjs.Bitmap, isVertical: boolean): void {
-        if (!isVertical) {
-            ship.x = 200;
-            ship.rotation = 90;
+    function rotate(ship: Ship): void {
+        if (!ship.isVertical) {
+            ship.shipObject.x = 200;
+            ship.shipObject.rotation = 90;
+            ship.isVertical = true;
         }
         else {
-            ship.y = 200;
-            ship.rotation = 0;
+            ship.shipObject.x = 0;            
+            ship.shipObject.rotation = 0;
+            ship.isVertical = false;
         }
+    }
+
+    function randomlyPlaceShips(): void {
+        var map: Array<Array<boolean>> = new Array(5);
+        for (var i = 0; i < 5; i++) {
+            map[i] = new Array(7);
+            for (var j = 0; j < 7; j++) {
+                map[i][j] = true;
+            }
+        }
+        for (var i = 0; i < 5; i++) {
+            if (Math.random() > 0.5) {
+                ships[i].isVertical = false;
+            }
+            else {
+                ships[i].isVertical = true;
+            }
+            rotate(ships[i]);
+            var limitX: number = 7;
+            var limitY: number = 5;
+            if (ships[i].isVertical) {
+                limitY -= ships[i].length;
+            }
+            else {
+                limitX -= ships[i].length;
+            }
+            var x: number;
+            var y: number;
+            do {
+                x = Math.floor(Math.random() * limitY);
+                y = Math.floor(Math.random() * limitX);
+            }while (!canShipBePlaced(map, x, y, ships[i].isVertical, ships[i].length));
+            if (ships[i].isVertical) {
+                for (var j = x;(x<5) && (j <= x + ships[i].length); j++) {
+                    map[j][y] = false
+                }
+            }
+            else {
+                for (var j = y; (y < 7) && (j <= y + ships[i].length); j++) {
+                    map[x][j] = false;
+                }
+            }
+            cells[x][y].addChild(ships[i].shipObject);
+        }
+    } 
+
+    function canShipBePlaced(map: Array<Array<boolean>>, x: number, y: number, isVertical: boolean, len: number): boolean {
+        if (isVertical) {
+            for (var i = x;(x < 5) && (i <= x + len); i++) {
+                if (map[i][y] == false) {
+                    return false
+                }
+            }
+        }
+        else {
+            for (var i = y; (y<7) && (i <= y + len); i++) {
+                if (map[x][i] == false) {
+                    return false
+                }
+            }
+        }
+        return true;
     }
 
     window.addEventListener("message", messageEventHandler, false);
